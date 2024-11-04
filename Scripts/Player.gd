a0b02f4d491c44e39dc220d7ed1da660c46c4eb8
Avt0r <ruslanmotriuk@gmail.com>
2024-11-04 14:27:24 +0300

    Add jump from wall

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 const SPEED = 100.0
-const JUMP_VELOCITY = -300.0
+const JUMP_VELOCITY = -350.0
 
 @onready var anim = $AnimatedSprite2D
 
@@ -11,31 +11,38 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and (is_on_floor() || is_on_wall()):
 		velocity.y = JUMP_VELOCITY
+		
+		if is_on_wall() && !is_on_floor():
+			if Input.is_action_pressed("Left"): 
+				velocity.x = (JUMP_VELOCITY / 2.1)
+			elif Input.is_action_pressed("Right"):
+				velocity.x = -(JUMP_VELOCITY / 2.1)
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if velocity.y >= 0:
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
 	
-	if velocity.length() == 0:
+	if !direction:
 		anim.play("Idle")
-	elif is_on_floor():
+	elif direction && is_on_floor_only():
 		anim.play("Run")
-	elif velocity.y <= 0:
+	elif velocity.y > 0:
 		anim.play("Jump")
-	else:
+	elif velocity.y < 0:
 		anim.play("Fall")
 	
 
-	if velocity.x < 0:
+	if Input.is_action_pressed("Right"):
 		anim.flip_h = true
 		anim.offset.x = -15
-	elif velocity.x > 0:
+	elif Input.is_action_pressed("Left"):
 		anim.flip_h = false
 		anim.offset.x = 0
 	
