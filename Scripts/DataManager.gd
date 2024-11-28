39170545a4193eb1d9bb497befd6bcf2ec29485d
Avt0r <ruslanmotriuk@gmail.com>
2024-11-28 15:25:28 +0300

    Add music

---
+++ Scripts/DataManager.gd
@@ -0,0 +1,13 @@
+extends Node
+
+var last_completed_level
+
+func _save():
+	var save_dict = {
+		"filename" : get_scene_file_path(),
+		"parent" : get_parent().get_path()
+	}
+	return save_dict
+
+func _load():
+	
